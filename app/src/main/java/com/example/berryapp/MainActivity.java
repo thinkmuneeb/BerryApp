6a454745cc2c9c4f53a6f1639cbc5e144bf12a92
Muneeb <thinkmuneeb@gmail.com>
2021-06-04 11:32:32 +0500

    values coming from internet

--- app/src/main/java/com/example/berryapp/MainActivity.java
+++ app/src/main/java/com/example/berryapp/MainActivity.java
@@ -4,9 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -28,10 +35,38 @@ public class MainActivity extends AppCompatActivity {
         button.setOnClickListener(v -> {
             textView.append(".");
 
+            //getWebcontent();
+
             // compare both the following functions, first one will hang UI second one will not
-            //timeTakingFunc();
-            asyncTimeTakingFunc();
+            // timeTakingFunc();
+             asyncTimeTakingFunc();
         });
+    }
+
+    String line = new String();
+    private void getWebcontent(){
+        try {
+            URL url = new URL("https://api.timezonedb.com/v2.1/get-time-zone?key=3ZN5USVAZ546&format=xml&by=zone&zone=Asia/Karachi");
+            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+            connection.connect();
+
+            String content = new String();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+            while(true){
+                line = reader.readLine();
+                if(line == null)
+                    break;
+                content += line;
+            }
+            line = content;
+            Log.i("TAG", content);
+            textView.setText(line);
+        }
+        catch (Exception e){
+            e.printStackTrace();
+            Log.i("TAG", "Error: " + e);
+        }
     }
 
     private void timeTakingFunc(){
@@ -43,9 +78,9 @@ public class MainActivity extends AppCompatActivity {
         AsyncTask<Integer,Integer,Integer> task = new AsyncTask<Integer,Integer,Integer>(){
             protected Integer doInBackground(Integer... params){
                 // do task
-                for(int j=0;j<10000000;j++)
+                getWebcontent();
+//                for(int j=0;j<10000000;j++)
                     i++;
-
                 return i;
             }
             protected void onPostExecute(Integer result){
