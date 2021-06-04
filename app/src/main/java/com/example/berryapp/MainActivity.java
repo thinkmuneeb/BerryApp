364df7c1ef8c420496259a5fd9ccc528769a0fce
Muneeb <thinkmuneeb@gmail.com>
2021-06-04 10:51:00 +0500

    async task practice done, nice and easy. Muneeb. 4 May 2021

--- app/src/main/java/com/example/berryapp/MainActivity.java
+++ app/src/main/java/com/example/berryapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.example.berryapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
@@ -11,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
+    Integer i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,6 +27,32 @@ public class MainActivity extends AppCompatActivity {
         button = (Button) findViewById(R.id.button);
         button.setOnClickListener(v -> {
             textView.append(".");
+
+            // compare both the following functions, first one will hang UI second one will not
+            //timeTakingFunc();
+            asyncTimeTakingFunc();
         });
+    }
+
+    private void timeTakingFunc(){
+        for(int j=0;j<10000000;j++)
+            i++;
+        textView.append(" " + i);
+    }
+    private void asyncTimeTakingFunc(){
+        AsyncTask<Integer,Integer,Integer> task = new AsyncTask<Integer,Integer,Integer>(){
+            protected Integer doInBackground(Integer... params){
+                // do task
+                for(int j=0;j<10000000;j++)
+                    i++;
+
+                return i;
+            }
+            protected void onPostExecute(Integer result){
+                // update UI
+                textView.append(" " + result);
+            }
+        };
+        task.execute();
     }
 }
