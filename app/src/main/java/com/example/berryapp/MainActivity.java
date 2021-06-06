636ac7c1bbc562e07fcb273145b86d71623d9c5c
Muneeb <thinkmuneeb@gmail.com>
2021-06-06 17:27:05 +0500

    made web content task class

--- app/src/main/java/com/example/berryapp/MainActivity.java
+++ app/src/main/java/com/example/berryapp/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.berryapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.AsyncTask;
+import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.widget.Button;
@@ -10,14 +11,15 @@ import android.widget.TextView;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.lang.ref.WeakReference;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.stream.Collectors;
 
 public class MainActivity extends AppCompatActivity {
 
     TextView textView;
     Button button;
-    Integer i = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -32,61 +34,67 @@ public class MainActivity extends AppCompatActivity {
         button = findViewById(R.id.button);
         button.setOnClickListener(v -> {
             textView.append(".");
-
-//            getWebContent();
-
             // compare both the following functions, first one will hang UI second one will not
-//            timeTakingFunc();
-             timeTakingFuncWithAsync();
+            // timeTakingFunc();
+            timeTakingFuncWithAsync();
         });
     }
 
-    String line = "";
-    private void getWebContent(){
-        try {
-            URL url = new URL("https://api.timezonedb.com/v2.1/get-time-zone?key=3ZN5USVAZ546&format=xml&by=zone&zone=Asia/Karachi");
-            HttpURLConnection connection = (HttpURLConnection) url.openConnection();
-            connection.connect();
-
-            String content = "";
-            BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
-
-            while(true){
-                line = reader.readLine();
-                if(line == null)
-                    break;
-                content = content.concat(line);
-            }
-            line = content;
-            Log.i("TAG", content);
-            textView.setText(line);
-        }
-        catch (Exception e){
-            e.printStackTrace();
-            Log.i("TAG", "Error: " + e);
-        }
-    }
-
-    private void timeTakingFunc(){
-        for(int j=0;j<10000000;j++)
-            i++;
-        textView.append(" " + i);
-    }
     private void timeTakingFuncWithAsync(){
-        getWebContent();
-        AsyncTask<Integer,Integer,Integer> task = new AsyncTask<Integer,Integer,Integer>(){
-            protected Integer doInBackground(Integer... params){
-                // do task
-//                getWebContent();
-//                for(int j=0;j<10000000;j++)
-                    i++;
-                return i;
+        new WebContentTask(this).execute();
+    }
+
+    // AsyncTask<Params, Progress, Result>
+    private static class WebContentTask extends AsyncTask<Void, Void, String> {
+
+        private final WeakReference<MainActivity> activityReference;
+
+        // only retain a weak reference to the activity
+        WebContentTask(MainActivity context) {
+            activityReference = new WeakReference<>(context);
+        }
+
+        @Override
+        protected String doInBackground(Void... voids) {
+            return getWebContent();
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+
+            // get a reference to the activity if it is still there
+            MainActivity activity = activityReference.get();
+            if (activity == null || activity.isFinishing()) return;
+
+            // modify the activity's UI
+            TextView textView = activity.findViewById(R.id.textView);
+            textView.append(result);
+
+            // access Activity member variables
+            // activity.mSomeMemberVariable = 321;
+        }
+
+        public String getWebContent(){
+            String line;
+            try {
+                URL url = new URL("https://api.timezonedb.com/v2.1/get-time-zone?key=3ZN5USVAZ546&format=xml&by=zone&zone=Asia/Karachi");
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                BufferedReader reader = new BufferedReader(new InputStreamReader(connection.getInputStream()));
+
+                line = Build.VERSION.SDK_INT >= Build.VERSION_CODES.N ?
+                        reader.lines().collect(Collectors.joining()) :
+                        "Please use Android Nougat.";
             }
-            protected void onPostExecute(Integer result){
-                // update UI
-                textView.append(" " + result);
+            catch (Exception e){
+                line = "some error";
+                e.printStackTrace();
+                Log.i("TAG", "Error: " + e);
             }
-        };
-        task.execute();
+
+            return line;
+        }
+
     }
 }
